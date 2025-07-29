e11f62a7f9047f2c6eee5c070180359b280198b5
Mohammed Tayeb <mohammedtayebibne@gmail.com>
2025-07-29 20:17:10 +0600

    logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import "../App.css";
 const Navbar = () => (
   <nav className="navbar">
     <div className="navbar-logo-group" style={{ display: 'flex', alignItems: 'center', gap: '0.75rem' }}>
-      <img src={logo} alt="BacheLORE Logo" style={{ height: '40px', width: '40px', objectFit: 'contain', borderRadius: '8px' }} />
+      <img src={logo} alt="BacheLORE Logo" style={{ height: '64px', width: '64px', objectFit: 'contain', borderRadius: '12px' }} />
       <span className="navbar-logo">BacheLORE</span>
     </div>
     <ul className="navbar-links">
