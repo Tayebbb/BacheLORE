6d82f9372772d2b1bf80fe5bf77be071bd2a36b9
tanjim@041 <tanjim433@gmail.com>
2025-08-23 14:23:43 +0600

    -update

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,5 @@ mongoose.connect(process.env.MONGO_URL)
 
 app.use("/api/auth", authRoutes);
 
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
